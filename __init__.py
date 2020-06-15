01911c93ac9de46f91ba9b7bde83429b388d1e1f
Tom Harris <tharris@harrisnj.net>
2020-06-14 22:17:15 -0400

    Fix known issues

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import logging
 from homeassistant.util.package import install_package
 
 try:
-    install_package("pyinsteon==1.0.3")
+    install_package("pyinsteon==1.0.4")
     from pyinsteon import async_close, async_connect, devices
 except:
     raise ImportError("Cannot install pyinsteon")
